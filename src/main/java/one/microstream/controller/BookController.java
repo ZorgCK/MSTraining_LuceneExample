b127f035a5a90d3e20fafe4343b48408a58905f2
Kümmel Christian <c.kuemmel@microstream.one>
2021-09-24 13:37:54 +0200

    zc

--- src/main/java/one/microstream/controller/BookController.java
+++ src/main/java/one/microstream/controller/BookController.java
@@ -1,16 +1,11 @@
 package one.microstream.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.lucene.queryparser.classic.ParseException;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import one.microstream.domain.Book;
-import one.microstream.domain.lucene.LuceneUtils;
 import one.microstream.storage.DB;
 import one.microstream.utils.MockupUtils;
 
@@ -32,39 +27,5 @@ public class BookController
 	public List<Book> getBook()
 	{
 		return DB.root.getBooks().all();
-	}
-	
-	@Get("/createIndex")
-	public HttpResponse<?> createLuceneIndex()
-	{
-		try
-		{
-			LuceneUtils.createIndex();
-		}
-		catch(IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return HttpResponse.ok("Index successfully cleared!");
-	}
-	
-	@Get("/searchLucene")
-	public HttpResponse<List<String>> searchWithLucene()
-	{
-		List<String> searchByTitle = new ArrayList<String>();
-		
-		try
-		{
-			searchByTitle = LuceneUtils.searchByTitle("star");
-		}
-		catch(IOException | ParseException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return HttpResponse.ok(searchByTitle);
 	}
 }
--- src/main/java/one/microstream/domain/Books.java
+++ src/main/java/one/microstream/domain/Books.java
@@ -1,13 +1,16 @@
 package one.microstream.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import one.microstream.persistence.types.Persister;
-import one.microstream.persistence.types.Storer;
 import one.microstream.reference.Lazy;
 import one.microstream.storage.DB;
 
@@ -34,31 +37,41 @@ public class Books
 	
 	private void add(final Book book, final Persister persister)
 	{
-		List<Book> list = isbnToBooks.get(book.getIsbn().charAt(0)).get();
+		List<Book> list = this.isbnToBooks.computeIfAbsent(
+			(String)String.valueOf(book.getIsbn().charAt(0)),
+			k -> Lazy.Reference(new ArrayList<>())).get();
 		list.add(book);
 		
-		List<Book> list2 = authorToBooks.get(book.getAuthor()).get();
+		List<Book> list2 = this.authorToBooks.computeIfAbsent(
+			(one.microstream.domain.Author)book.getAuthor(),
+			k -> Lazy.Reference(new ArrayList<>())).get();
 		list2.add(book);
 		
-		persister.storeAll(list, list2);
+		persister.storeAll(list, list2, authorToBooks, isbnToBooks);
 	}
 	
 	private void addAll(final Collection<? extends Book> books, final Persister persister)
 	{
-		Storer storer = persister.createEagerStorer();
+		Set<Object> storeTargets = new HashSet<>();
 		
 		books.forEach(b ->
 		{
-			List<Book> list = isbnToBooks.get(b.getIsbn().charAt(0)).get();
+			List<Book> list = this.isbnToBooks.computeIfAbsent(
+				(String)String.valueOf(b.getIsbn().charAt(0)),
+				k -> Lazy.Reference(new ArrayList<>())).get();
 			list.add(b);
 			
-			List<Book> list2 = authorToBooks.get(b.getAuthor()).get();
+			List<Book> list2 = this.authorToBooks.computeIfAbsent(
+				(one.microstream.domain.Author)b.getAuthor(),
+				k -> Lazy.Reference(new ArrayList<>())).get();
 			list2.add(b);
 			
-			storer.storeAll(list, list2);
+			storeTargets.addAll(Arrays.asList(list, list2));
 		});
 		
-		storer.commit();
+		storeTargets.addAll(Arrays.asList(authorToBooks, isbnToBooks));
+		
+		persister.storeAll(storeTargets);
 	}
 	
 }
